7374d0dc10511be2d9e34ec9d18f5a45ce2c9418
song04 <songhwoo11@gmail.com>
2026-09-22 22:50:24 +0900

    LEC06 Character 생성자 구현 및 테스트 검증

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -17,7 +17,11 @@ from pico2d import *
 # - moveCircle: 원 경로로 이동
 # - moveTriangle: 삼각형 경로로 이동
 class Character:
-    pass
+    def __init__(self, imagefilename, x, y):
+        self.image = load_image(imagefilename)
+        self.x = x
+        self.y = y
+        self.move_flag = 1
 
 # TODO: 배경매니저 클래스 제작
 # 인스턴스 속성
@@ -33,15 +37,19 @@ class Character:
 class BgManager:
     pass
 
-open_canvas(800, 600)
-
 # TODO: 원 -> 사각형 -> 삼각형 운동 반복하는 코드 만들기
 # 구현 아이디어: Character의 움직임 플래그를 이용하여 작동 시켜라
 # 원 - 1, 사각형 - 2, 삼각형 - 3으로 두고 원래 자리로 돌아올 때마다 플래그를 바꿔준다
+open_canvas(800, 600)
+character = Character('character.png', 400, 300)
 
+# 테스트용
 while True:
     clear_canvas()
+    character.image.draw(character.x,character.y)
     update_canvas()
+    print(f"Character Position: ({character.x}, {character.y})")
+    print(f"움직임 플래그: {character.move_flag}")
     
-    delay(0.01)
-close_canvas()
+    delay(5) # 테스트를 위해 5초로 설정. 실제로는 0.01초로 설정해야 함
+    close_canvas() # 테스트를 위해 안에 넣어둠. 실제로는 while문 밖에 있어야 함
