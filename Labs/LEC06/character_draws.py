289db93b47b166d8b95290fb2e84ffbd3ea6b19a
song04 <songhwoo11@gmail.com>
2026-09-22 23:13:07 +0900

    LEC06 BgManager.append 구현 및 테스트 검증

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -40,12 +40,19 @@ class Character:
 class BgManager:
     def __init__(self):
         self.backgrounds = []
+    def append(self, imagefilename, x, y):
+        image = load_image(imagefilename)
+        self.backgrounds.append((image, x, y))
 
 # TODO: 원 -> 사각형 -> 삼각형 운동 반복하는 코드 만들기
 # 구현 아이디어: Character의 움직임 플래그를 이용하여 작동 시켜라
 # 원 - 1, 사각형 - 2, 삼각형 - 3으로 두고 원래 자리로 돌아올 때마다 플래그를 바꿔준다
 open_canvas(800, 600)
 character = Character('character.png', 400, 300)
+bgmanager = BgManager()
+bgmanager.append('sky.png', 400, 300)
+bgmanager.append('grass.png', 400, 30)
+
 grass = load_image('grass.png')
 sky = load_image('sky.png')
 
@@ -58,6 +65,8 @@ while True:
     update_canvas()
     print(f"Character Position: ({character.x}, {character.y})")
     print(f"움직임 플래그: {character.move_flag}")
+    for background in bgmanager.backgrounds:
+        print(background)
 
     delay(5) # 테스트를 위해 5초로 설정. 실제로는 0.01초로 설정해야 함
     close_canvas() # 테스트를 위해 안에 넣어둠. 실제로는 while문 밖에 있어야 함
