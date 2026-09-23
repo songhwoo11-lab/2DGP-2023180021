88d87ba9ddcf6cd13f4a65c28455858573cf21d4
song04 <songhwoo11@gmail.com>
2026-09-23 16:41:04 +0900

    LEC06 삼각형 한 바퀴 완료 후 이동 정지 로직 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -79,6 +79,7 @@ class Character:
             self.moveDownLeft(1)    
         else:
             count = 0
+            self.move_flag = 0
             return
         count += 1
     def moveUpLeft(self, x):
@@ -110,7 +111,7 @@ bgmanager = BgManager()
 bgmanager.append('sky.png', 400, 300)
 bgmanager.append('grass.png', 400, 30)
 x, y, theta, r, count = 400, 300, 0, 100, 0
-character.move_flag = 2
+character.move_flag = 3
 
 # 테스트용
 while True:
